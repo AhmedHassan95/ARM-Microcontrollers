c2eee94b0f768f39b07f88cedc180e47d457c130
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-11-29 09:49:21 +0200

    Update TuningFork.c

--- Embedded Systems - Shape The World Multi-Threaded Interfacing/Lab12_TuningFork/TuningFork.c
+++ Embedded Systems - Shape The World Multi-Threaded Interfacing/Lab12_TuningFork/TuningFork.c
@@ -50,25 +50,25 @@ volatile unsigned long flag = 0;
 /* Input from PA3, output from PA2 */
 void Sound_Init(void)
 { 
-	unsigned long volatile delay;	
-	SYSCTL_RCGC2_R |= 0x00000001; 		// Activate clock for port A
-	delay = SYSCTL_RCGC2_R;  
-	time = 90909;               	  	// 880 Hz
-  GPIO_PORTA_AMSEL_R &= ~0x0C;      // Disable analog functionality on PA3, PA2
-  GPIO_PORTA_PCTL_R &= ~0x0000FF00; // Configure PA3, PA2 as GPIO
+  unsigned long volatile delay;	
+  SYSCTL_RCGC2_R |= 0x00000001; 		// Activate clock for port A
+  delay = SYSCTL_RCGC2_R;  
+  time = 90909;               	  		// 880 Hz
+  GPIO_PORTA_AMSEL_R &= ~0x0C;      		// Disable analog functionality on PA3, PA2
+  GPIO_PORTA_PCTL_R &= ~0x0000FF00; 		// Configure PA3, PA2 as GPIO
   GPIO_PORTA_DIR_R = 0x04;     			// Make PA2 output, and PA3 input
   GPIO_PORTA_DR8R_R |= 0x04;    		// Enable 8 mA drive on PA2
   GPIO_PORTA_AFSEL_R &= ~0x0C;  		// Disable alt funct on PA3, PA2
-	GPIO_PORTA_PDR_R = 0x08;  				// Activate the Pull down resistor for PA3
-	GPIO_PORTA_DEN_R |= 0x0C;     		// Enable digital I/O on PA3, PA2
+  GPIO_PORTA_PDR_R = 0x08;  			// Activate the Pull down resistor for PA3
+  GPIO_PORTA_DEN_R |= 0x0C;     		// Enable digital I/O on PA3, PA2
   GPIO_PORTA_DATA_R &= ~0x0C;   		// Make PA2 low
 }
 
 /* SysTick timer Initialization */
 void Systick_Init(void)
 { 
-	NVIC_ST_CTRL_R = 0;           		// Disable SysTick during setup
-  NVIC_ST_RELOAD_R = time-1;        // Reload value for 880 Hz
+  NVIC_ST_CTRL_R = 0;           		// Disable SysTick during setup
+  NVIC_ST_RELOAD_R = time-1;       		// Reload value for 880 Hz
   NVIC_ST_CURRENT_R = 0;        		// Any write to current clears it
   NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R & 0x00FFFFFF); // priority 0
   NVIC_ST_CTRL_R = 0x00000007;  		// Enable with core clock and interrupts
@@ -107,14 +107,14 @@ void SysTick_Handler(void)
 
 int main(void)
 {
-	/* Activate grader and set system clock to 80 MHz */
-	TExaS_Init(SW_PIN_PA3, HEADPHONE_PIN_PA2,ScopeOn); 
+   /* Activate grader and set system clock to 80 MHz */
+  TExaS_Init(SW_PIN_PA3, HEADPHONE_PIN_PA2,ScopeOn); 
   Sound_Init();         
   Systick_Init();
-	EnableInterrupts();   /* Enable after all initialization are done */
+  EnableInterrupts();   /* Enable after all initialization are done */
   
-	while(1)
-	{
+  while(1)
+  {
     /* Main program is free to perform other tasks */
     /* Do not use WaitForInterrupt() here, it may cause the TExaS to crash */
   }
