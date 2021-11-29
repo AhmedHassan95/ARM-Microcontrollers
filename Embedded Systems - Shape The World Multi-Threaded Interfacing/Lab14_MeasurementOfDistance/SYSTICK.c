06a74522ca7b5f95ca1011f762ce37fd1fb4ec7c
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-11-29 10:02:19 +0200

    Update SYSTICK.c

--- Embedded Systems - Shape The World Multi-Threaded Interfacing/Lab14_MeasurementOfDistance/SYSTICK.c
+++ Embedded Systems - Shape The World Multi-Threaded Interfacing/Lab14_MeasurementOfDistance/SYSTICK.c
@@ -3,11 +3,11 @@
 // Initialize SysTick interrupts to trigger at 40 Hz, 25 ms
 void SysTick_Init(unsigned long period)
 {
- NVIC_ST_CTRL_R = 0;		// Disable SysTick during setup
- NVIC_ST_CURRENT_R = 0; // Any write to current clears it
- NVIC_ST_RELOAD_R = period-1 ; 
- NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R & 0x00FFFFFF) | 0x20000000; // Priority 1
- NVIC_ST_CTRL_R = 0x007; 	// Enable SysTick with core clock and interrupts
+	 NVIC_ST_CTRL_R = 0;		// Disable SysTick during setup
+	 NVIC_ST_CURRENT_R = 0; // Any write to current clears it
+	 NVIC_ST_RELOAD_R = period-1 ; 
+	 NVIC_SYS_PRI3_R = (NVIC_SYS_PRI3_R & 0x00FFFFFF) | 0x20000000; // Priority 1
+	 NVIC_ST_CTRL_R = 0x007; 	// Enable SysTick with core clock and interrupts
 }
 
 // executes every 25 ms, collects a sample, converts and stores in mailbox
