b44bedbc245fc6b4e3204cd752fce69b97ee58b2
Ahmed Hassan <ahmed.hassan9575@gmail.com>
2021-11-29 10:01:20 +0200

    Update LED.c

--- Embedded Systems - Shape The World Multi-Threaded Interfacing/Lab14_MeasurementOfDistance/LED.c
+++ Embedded Systems - Shape The World Multi-Threaded Interfacing/Lab14_MeasurementOfDistance/LED.c
@@ -5,14 +5,14 @@
 // Output: None
 void LED_Init(void)
 { 
-	volatile unsigned long delay;
-  SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF; /* 1) Activate clock for Port F */
+  volatile unsigned long delay;
+  SYSCTL_RCGC2_R |= SYSCTL_RCGC2_GPIOF; 		/* 1) Activate clock for Port F */
   delay = SYSCTL_RCGC2_R;         			/* allow time for clock to stabilize */
-  GPIO_PORTF_DIR_R |= 0x02;      				/* 2) Make PF2 output */
+  GPIO_PORTF_DIR_R |= 0x02;      			/* 2) Make PF2 output */
   GPIO_PORTF_AFSEL_R &= ~0x02;     			/* 3) Disable alternate function on PF2 */
-  GPIO_PORTF_DEN_R |= 0x02;      				/* 4) Enable digital I/O on PF2 */
+  GPIO_PORTF_DEN_R |= 0x02;      			/* 4) Enable digital I/O on PF2 */
   GPIO_PORTF_AMSEL_R &= ~0x02;     			/* 5) Disable analog function on PF2 */
-	GPIO_PORTF_DATA_R &= ~0x02;     			/* 6) Turn off LED at the beginning */
+  GPIO_PORTF_DATA_R &= ~0x02;     			/* 6) Turn off LED at the beginning */
 }
 
 // Function to toggle the LED on PORTF pin 1
